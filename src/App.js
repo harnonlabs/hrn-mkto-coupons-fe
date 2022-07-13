8b75225a52f67cceb5440a5e33b083e270e1ee67
sebasospina343 <sebasospina343@gmail.com>
2022-07-13 08:57:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,222 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from "react"
+import { Typography, TextField, Button, Box } from "@mui/material"
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+import ListItemAvatar from "@mui/material/ListItemAvatar"
+import ListSubheader from "@mui/material/ListSubheader"
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"
+import Divider from "@mui/material/Divider"
+import NewReleasesIcon from "@mui/icons-material/NewReleases"
+import H from "./img/H.svg"
 
 function App() {
+  const fileReader = new FileReader()
+  const [file, setFile] = React.useState()
+  const [csvToJSON, setCsvToJSON] = React.useState([])
+  const [coupon, setCoupon] = React.useState()
+  const [isSubmitDone, setIsSubmitDone] = React.useState(false)
+  const [couponTest, setCouponTest] = React.useState()
+  const [couponError, setCouponError] = React.useState()
+
+  const csvToObj = string => {
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n")
+
+    const result = csvRows.reduce((acc, cur) => {
+      const [coupon, isUsed] = cur.split(";")
+      if (coupon === "") {
+        return acc
+      }
+      return { ...acc, [coupon]: { isUsed, date: null } }
+    }, {})
+
+    setCsvToJSON(result)
+
+    return result
+  }
+
+  const handleOnChange = e => {
+    setFile(e.target.files[0])
+  }
+
+  const sendToServer = async x => {
+    try {
+      const request = await fetch(
+        `https://hrn-mkto-coupons.harnonlabs.workers.dev/load`,
+        {
+          method: "POST",
+          body: JSON.stringify({ csv: x }),
+          headers: { "content-type": "application/json" },
+        }
+      )
+      const response = await request.json()
+      return response
+    } catch (err) {
+      console.log("ERROR!", err)
+    }
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if (file) {
+      fileReader.onload = async function (event) {
+        const csvOutput = event.target.result
+        const json = csvToObj(csvOutput)
+        const response = await sendToServer(json)
+        if (response.message) {
+          setIsSubmitDone(true)
+        }
+      }
+
+      fileReader.readAsText(file)
+    }
+  }
+
+  const handleChangeCoupon = e => {
+    setCoupon(e.target.value)
+  }
+
+  const handleSubmitCoupon = async e => {
+    e.preventDefault()
+
+    try {
+      const request = await fetch(
+        `https://hrn-mkto-coupons.harnonlabs.workers.dev/spend?coupon=${coupon}`,
+        {
+          headers: { "content-type": "application/json" },
+        }
+      )
+      const response = await request.json()
+      console.log(response)
+      if (response.status === "error") {
+        setCouponError(response.errorMessage)
+      }
+      if (response.coupon) {
+        setCouponTest(response.coupon)
+      }
+      return response
+    } catch (err) {
+      console.log("ERROR!", err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Box sx={{ mr: 2 }}>
+              <img src={H} width="40" sx={{ mr: 2 }} alt="Harnon.co" />
+            </Box>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              HRN MARKETO COUPONS
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <div className="App">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Typography variant="h2" sx={{ mt: 2, mb: 2 }}>
+            Load coupons
+          </Typography>
+          <Typography variant="body1">
+            Load your CSV file with all your coupons included
+          </Typography>
+          <form name="coupons" id="couponsForm">
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "50em",
+              }}
+            >
+              <input
+                type={"file"}
+                accept={".csv"}
+                id={"csvFileInput"}
+                onChange={handleOnChange}
+              />
+
+              <Button
+                variant="contained"
+                onClick={handleSubmit}
+                disabled={isSubmitDone}
+              >
+                {isSubmitDone ? "Coupons succesfully loaded!" : "Load coupons!"}
+              </Button>
+            </Box>
+          </form>
+
+          <form name="getCoupon" id="getCoupon">
+            <Divider />
+            <List
+              sx={{
+                width: "100%",
+                maxWidth: 360,
+                bgcolor: "background.paper",
+                mt: 5,
+              }}
+              subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                  Test one of your uploaded coupons
+                </ListSubheader>
+              }
+            >
+              <ListItem>
+                <ListItemText>
+                  <TextField
+                    label="Test a coupon"
+                    sx={{ margin: "10px" }}
+                    onChange={handleChangeCoupon}
+                    name="coupon"
+                  />
+                </ListItemText>
+                <ListItemAvatar>
+                  <Button variant="contained" onClick={handleSubmitCoupon}>
+                    Test!
+                  </Button>
+                </ListItemAvatar>
+              </ListItem>
+              {couponTest && (
+                <ListItem>
+                  <ListItemText
+                    primary={couponTest}
+                    secondary="Ready to connect to Marketo!"
+                  />
+                  <ListItemAvatar>
+                    <CheckCircleIcon color="success" sx={{ fontSize: 40 }} />
+                  </ListItemAvatar>
+                </ListItem>
+              )}
+              {couponError && (
+                <ListItem>
+                  <ListItemText
+                    primary={couponError}
+                    secondary="Try uploading your coupons list again"
+                    color="error"
+                  />
+                  <ListItemAvatar>
+                    <NewReleasesIcon color="error" sx={{ fontSize: 40 }} />
+                  </ListItemAvatar>
+                </ListItem>
+              )}
+            </List>
+          </form>
+        </Box>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { purple } from "@mui/material/colors"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2C2A4A",
+    },
+    secondary: {
+      main: "#4F518C",
+    },
+  },
+  success: "#7FDEFF",
+  error: "#DABFFF",
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
