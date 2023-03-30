f4f819e112fd1ea12bb1e9edf22b4c7cf909b888
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2023-03-30 11:16:00 -0500

    add company selector

--- src/screens/GlobalUsersScreen/GlobalUsersScreen.js
+++ src/screens/GlobalUsersScreen/GlobalUsersScreen.js
@@ -1,33 +1,57 @@
 import * as React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
-import { Typography, Box, Chip } from '@mui/material';
+import { Typography, Box, Paper, TextField, Button } from '@mui/material';
 import { AppContext } from '../../App';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import { ResetTvOutlined } from '@mui/icons-material';
 import Snackbar from '@mui/material/Snackbar';
 import EditIcon from '@mui/icons-material/Edit';
 import VerifiedIcon from '@mui/icons-material/Verified';
+import Modal from '@mui/material/Modal';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function GlobalUsersSCreen() {
-  const { user, isLoading } = useAuth0();
+  const { user } = useAuth0();
   const [openSnackbar, setOpenSnackbar] = React.useState(false);
-  const [copiedKey, setCopiedKey] = React.useState('');
   const appContext = React.useContext(AppContext);
   const [data, setData] = React.useState([]);
   const [dataFlag, setDataFlag] = React.useState(false);
   const [companyName, setCompanyName] = React.useState('');
   const [columna, setColumna] = React.useState([]);
-
+  const [isSubmitDone, setIsSubmitDone] = React.useState(false);
+  const [companyList, setCompanyList] = React.useState([]);
+  const [companyFlag, setCompanyFlag] = React.useState(false);
+  const [openModalUserCompany, setOpenModalUserCompany] = React.useState(false);
+  const handleOpenModalUserCompany = () => setOpenModalUserCompany(true);
+  const handleCloseModalUserCompany = () => setOpenModalUserCompany(false);
+  const [selectedCompany, setSelectedCompany] = React.useState('');
+  const [selectedEmail, setSelectedEmail] = React.useState('');
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  React.useEffect(() => {
+    if (companyList.length > 0) {
+      setCompanyFlag(true);
+    }
+  }, [companyList]);
   React.useEffect(() => {
     setColumna([
       { field: 'email', headerName: 'Email', width: 350 },
-      { field: 'company', headerName: 'Company', width: 100 },
+      { field: 'company', headerName: 'Company', width: 200 },
       { field: 'approver', headerName: 'Approver', width: 80 },
       {
-        field: 'actions',
+        field: 'actions1',
         type: 'actions',
-        width: 100,
+        headerName: 'Set approver',
+        width: 150,
         getActions: (params) => [
           <GridActionsCellItem
             icon={<VerifiedIcon />}
@@ -38,12 +62,26 @@ export default function GlobalUsersSCreen() {
           />,
         ],
       },
+      {
+        field: 'actions2',
+        type: 'actions',
+        headerName: 'Select company',
+        width: 150,
+        getActions: (params) => [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="SetApprover"
+            onClick={() => {
+              selectCompany(params.row['id']);
+            }}
+          />,
+        ],
+      },
     ]);
   }, [user.email]);
   React.useEffect(() => {
     async function getData() {
       try {
-        console.log('columna', columna);
         const request = await fetch(
           `${process.env.REACT_APP_WORKER_URL}/listUsers`,
           {
@@ -60,7 +98,7 @@ export default function GlobalUsersSCreen() {
             (item) => item.email === user.email,
           );
 
-          setCompanyName('global Harnon configuration');
+          //  setCompanyName('global Harnon configuration');
           let finalArr = [];
 
           finalArr = response.map((cl) => {
@@ -79,19 +117,13 @@ export default function GlobalUsersSCreen() {
       }
     }
     getData();
-  }, [appContext.token, companyName, user.email]);
+  }, [appContext.token, user.email]);
   React.useEffect(() => {
     if (data.length > 0) {
+      getCompanyList();
       setDataFlag(true);
     }
   }, [data]);
-  const copyToClipboard = (e) => {
-    let copyText = e.target;
-
-    navigator.clipboard.writeText(copyText.textContent);
-    setCopiedKey(copyText.textContent);
-    setOpenSnackbar(true);
-  };
 
   const onCloseSnackbar = () => {
     setOpenSnackbar(false);
@@ -113,14 +145,12 @@ export default function GlobalUsersSCreen() {
         const userResponse = response.find((item) => item.email === user.email);
         setCompanyName(userResponse.companyName);
         let finalArr = [];
-        const filterCompanyUsers = response.filter(
-          (item) => item.companyName === companyName,
-        );
 
-        finalArr = filterCompanyUsers.map((cl) => {
+        finalArr = response.map((cl) => {
           return {
             id: cl.email,
             email: cl.email,
+            company: cl.companyName,
             approver: cl.approver,
           };
         });
@@ -151,20 +181,149 @@ export default function GlobalUsersSCreen() {
       console.log('ERROR!', err);
     }
   };
-  console.log('REACT_APP_WORKER_URL', process.env.REACT_APP_WORKER_URL, 'no');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_WORKER_URL}/createCompany`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            content: {
+              email: user.email,
+              token: appContext.token,
+              companyName,
+            },
+          }),
+          headers: { 'content-type': 'application/json' },
+        },
+      );
+      const response = await request.json();
+      setCompanyName('');
+      setIsSubmitDone(true);
+      getCompanyList();
+      return response;
+    } catch (err) {
+      console.log('ERROR!', err);
+    }
+  };
+
+  const handleSubmitModal = async () => {
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_WORKER_URL}/pickCompany`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            content: {
+              email: selectedEmail,
+              token: appContext.token,
+              companyName: selectedCompany,
+            },
+          }),
+          headers: { 'content-type': 'application/json' },
+        },
+      );
+      const response = await request.json();
+      handleCloseModalUserCompany();
+      updateData();
+      return response;
+    } catch (err) {
+      console.log('ERROR!', err);
+    }
+  };
+  const setCompany = (name) => {
+    setCompanyName(name);
+    setIsSubmitDone(false);
+  };
+  const selectCompany = async (email) => {
+    setSelectedEmail(email);
+    handleOpenModalUserCompany();
+  };
+
+  const getCompanyList = async () => {
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_WORKER_URL}/listCompanies`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            content: { token: appContext.token, email: user.email },
+          }),
+          headers: { 'content-type': 'application/json' },
+        },
+      );
+      const response = await request.json();
+      if (response) {
+        const array = response.map((cl) => {
+          return cl.companyName;
+        });
+
+        setCompanyList(array);
+      }
+    } catch (err) {
+      console.log('ERROR!', err);
+    }
+  };
+  const handleChangeSelector = (event) => {
+    setSelectedCompany(event.target.value);
+  };
+
+  // console.log('REACT_APP_WORKER_URL', process.env.REACT_APP_WORKER_URL, 'no');
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      {/* {dataFlag && <p>({data[0].email})</p>} */}
+      <>
+        <Typography variant="h4" sx={{ mt: 2, mb: 2 }}>
+          Create Company
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Paper
+            sx={{
+              width: '35%',
+              padding: '2rem',
+            }}
+            elevation={4}
+          >
+            <Box>
+              <form>
+                <Box>
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Company Name"
+                    sx={{ mb: '1rem' }}
+                    onChange={(e) => setCompany(e.target.value)}
+                  />
+                </Box>
+                <Button variant="contained" onClick={handleSubmit}>
+                  {'Create Company!'}
+                </Button>
+                {isSubmitDone && (
+                  <Typography variant="h6" sx={{ mt: 2, mb: 2 }}>
+                    Created!.
+                  </Typography>
+                )}
+              </form>
+            </Box>
+          </Paper>
+        </Box>
+      </>
       {dataFlag && (
         <Box key={`dg-user`}>
           <Typography variant="h4" sx={{ mt: '1rem', textAlign: 'left' }}>
-            {`${companyName} Users`}
+            {`Harnon Global Users`}
           </Typography>
           <Box sx={{ display: 'flex', mt: 2, mb: 2 }}></Box>
-          {/* <Typography variant="h6" sx={{ textAlign: "left" }}>
-                {cl.key}
-              </Typography> */}
+
           <DataGrid
             rows={data}
             columns={columna}
@@ -179,6 +338,44 @@ export default function GlobalUsersSCreen() {
           />
         </Box>
       )}
+      <Modal
+        open={openModalUserCompany}
+        onClose={handleCloseModalUserCompany}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Select Company
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Please assing a company to the user.
+            </Typography>
+          </Box>
+          <Box>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={selectedCompany}
+              label="Company"
+              onChange={handleChangeSelector}
+            >
+              {companyFlag &&
+                companyList.map((item) => {
+                  return (
+                    <MenuItem key={item} value={item}>
+                      {item}
+                    </MenuItem>
+                  );
+                })}
+            </Select>
+          </Box>
+          <Button variant="contained" onClick={handleSubmitModal}>
+            {'Select Company!'}
+          </Button>
+        </Box>
+      </Modal>
       <Snackbar
         open={openSnackbar}
         autoHideDuration={1000}
