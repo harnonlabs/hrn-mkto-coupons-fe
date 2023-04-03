1e0255bdb98726cf76daf00d1d74f29fa468117f
Sebastian Ospina <sebasospina343@users.noreply.github.com>
2023-04-03 10:40:43 -0400

    console log environment

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { Auth0Provider } from "@auth0/auth0-react"
+import { BrowserRouter } from "react-router-dom"
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2C2A4A',
+      main: "#2C2A4A",
     },
     secondary: {
-      main: '#4F518C',
+      main: "#4F518C",
     },
   },
-  success: '#7FDEFF',
-  error: '#DABFFF',
-});
+  success: "#7FDEFF",
+  error: "#DABFFF",
+})
+console.log("process.env.REACT_APP_FE_URL", process.env.REACT_APP_FE_URL)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <Auth0Provider
     domain="harnonlabs.us.auth0.com"
@@ -35,10 +36,10 @@ root.render(
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  </Auth0Provider>,
-);
+  </Auth0Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
